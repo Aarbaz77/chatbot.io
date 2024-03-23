2bc32aadd8a1d62c8d5aaaf0d275e76640df8851
Aarbaz Alam <39804950+Aarbaz77@users.noreply.github.com>
2024-03-23 03:02:45 +0000

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
 const action_name = "action_hello_world";
 // const rasa_server_url = "https://localhost:5055/webhook";
-const rasa_server_url = "http://54.243.1.12:5005/webhooks/rest/webhook";
+const rasa_server_url = "https://54.243.1.12:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
